f93e3280cd47e04da7b3a26a1419bcfdf021f67d
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2018-06-25 15:32:53 +0530

    fixed issue #1

--- js/delRepo.js
+++ js/delRepo.js
@@ -2,4 +2,4 @@ $repoName=$("#rename_field").val();
 console.log($repoName);
 $(".Box-row:nth-child(4) .boxed-action").click();
 $(".input-block").val($repoName);
-$delBtn = $(".btn-danger:contains('I understand the consequences, delete this repository')").click();
+$delBtn = $(".btn-danger:contains('I understand the consequences, delete this repository')").removeAttr("disabled").click();

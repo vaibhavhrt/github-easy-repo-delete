33c3061b8933e2904bc61f5c2dd713a0987dd9d2
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2018-06-26 18:20:36 +0530

    Close tabs after repo deletion

--- js/background.js
+++ js/background.js
@@ -1,10 +1,27 @@
+var newTabId;
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse){
-        sendResponse({farewell: "Received link"+ request.repo});
         chrome.tabs.create({
             url: "https://www.github.com/"+request.repo+"/settings?egrd=True",
             active: false
+        }, function(tab){
+            console.log("Created new tab with id: "+tab.id+" to delete the github repo: "+request.repo);
+            test(tab.id, newTabId);
+            console.log("Deleteing Github Repo ... Please Wait ...");
+        });
+        sendResponse({
+            farewell: "Received link"+ request.repo
         });
     return true;
     }
 );
+
+function test(tabId){
+    newTabId = tabId;
+}
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+    if(changeInfo.url === "https://github.com/" && tabId===newTabId){
+        console.log("Github Repo Deleted Successfully, removing tab: "+newTabId);
+        chrome.tabs.remove(newTabId);
+    }
+});

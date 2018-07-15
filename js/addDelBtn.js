7887f5b00eec7873ae120d4e4b2305fe5ad23158
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2018-07-15 15:59:44 +0530

    Done #4, now multiple repos can be deleted at once

--- js/addDelBtn.js
+++ js/addDelBtn.js
@@ -1,12 +1,35 @@
+//add delete all selected repos button
+$('<div class="border-bottom border-gray-dark py-3"><button class="egrd btn btn-sm btn-danger">Delete Selected Repos</button></div>').insertAfter('.border-bottom.border-gray-dark.py-3');
+
 $link ='';
 $("#user-repositories-list .border-bottom").each(function() {
     $link=$(this).find("h3 a").attr("href");
-    $("<button data-repo="+$link+" class='egrd'>Delete Repo</button>").appendTo(this).children("div.f6.text-gray.mt-2")
+    $("<input type='checkbox' value="+$link+" class='egrd'/>").appendTo(this);
+    $('<span class="label" style="color: red">Delete Repo</span>').appendTo(this);
 });
+
+//get list of selected repos
+$links = [];
 $('button.egrd').on('click', function(){
-    $link = $(this).attr("data-repo");
-    console.log("Button clicked, link sent to background :" + $link);
-    chrome.runtime.sendMessage({repo: $link}, function(response){
+    $('.egrd:checkbox:checked').each(function(){
+        $l = $(this).val();
+        $links.push($l);
+        console.log($l);
+    });
+    console.log($links);
+    //$link = $(this).attr("data-repo");
+    //console.log("Button clicked, link sent to background :" + $link);
+    chrome.runtime.sendMessage({repo: $links}, function(response){
         console.log(response.farewell);
     });
+    $links = [];
+});
+
+//listen for sucussful deletion of repos
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.greeting == "DEL OK"){
+            console.log("Successfully deleted all repos!")
+            sendResponse({farewell: "THANKS"});
+        }
 });
--- js/background.js
+++ js/background.js
@@ -1,27 +1,52 @@
 var newTabId;
+var linksToDelete = [];
+var i;
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse){
-        chrome.tabs.create({
-            url: "https://www.github.com/"+request.repo+"/settings?egrd=True",
-            active: false
-        }, function(tab){
-            console.log("Created new tab with id: "+tab.id+" to delete the github repo: "+request.repo);
-            test(tab.id, newTabId);
-            console.log("Deleteing Github Repo ... Please Wait ...");
-        });
+        linksToDelete = request.repo;
+        i = 0;
+        deleteRepos(linksToDelete[i]);
         sendResponse({
-            farewell: "Received link"+ request.repo
+            farewell: "Received links Deleting " + linksToDelete.length + " repos"
         });
     return true;
     }
 );
 
-function test(tabId){
-    newTabId = tabId;
+//delete one repo at a time
+function deleteRepos(link){
+    chrome.tabs.create({
+        url: "https://www.github.com/"+link+"/settings?egrd=True",
+        active: false
+    }, function(tab){
+        console.log("Created new tab with id: "+tab.id+" to delete the github repo: "+link);
+        getTabId(tab.id, newTabId);
+        console.log("Deleteing Github Repo ... Please Wait ...");
+    });
 }
+
+//once a repo a deleted, call function to delete next repo
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
     if(changeInfo.url === "https://github.com/" && tabId===newTabId){
         console.log("Github Repo Deleted Successfully, removing tab: "+newTabId);
         chrome.tabs.remove(newTabId);
+        //Check if there is any more repos to deleted
+        if(i < linksToDelete.length-1){
+            i+=1;
+            deleteRepos(linksToDelete[i])
+        }
+        //send message that all repos successfully deleted
+        else{
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+                chrome.tabs.sendMessage(tabs[0].id, {greeting: "DEL OK"}, function(response) {
+                    console.log(response.farewell);
+                });  
+            });
+        }
     }
 });
+
+//get tabId of newly creted tab
+function getTabId(tabId){
+    newTabId = tabId;
+}

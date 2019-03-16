ec1a76e961326e34effd0922c6b9e4b42416f316
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2019-03-17 00:43:27 +0530

    instructions on how to get token

--- src/index/tokenInput/tokenInputComponent.js
+++ src/index/tokenInput/tokenInputComponent.js
@@ -5,6 +5,10 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Tooltip from '@material-ui/core/Tooltip';
+
+// Icons
+import HelpIcon from '@material-ui/icons/Help';
 
 const styles = theme => ({
     root: {
@@ -15,35 +19,46 @@ const styles = theme => ({
     },
     submit: {
         margin: `0 ${theme.spacing.unit}px`,
+        display: 'flex',
+        alignItems: 'center',
+    },
+    tooltip: {
+        margin: `0 ${theme.spacing.unit}px`,
     },
 });
 
 const TokenInputComponent = props => {
     const { classes, form, onSubmit, onChange } = props;
     return (
-        <div className={classes.root}>
-            <form onSubmit={onSubmit}>
-                <TextField
-                    required
-                    variant='outlined'
-                    id='id_token'
-                    name='token'
-                    label='API Token'
-                    placeholder='Enter Github API Token'
-                    value={form.data.token || ''}
-                    onChange={onChange}
-                />
+        <form onSubmit={onSubmit} className={classes.root}>
+            <TextField
+                required
+                variant='outlined'
+                id='id_token'
+                name='token'
+                label='API Token'
+                placeholder='Enter Github API Token'
+                value={form.data.token || ''}
+                onChange={onChange}
+            />
+            <div className={classes.submit}>
                 <Button
                     type='submit'
                     color='primary'
                     variant='contained'
                     size='large'
-                    className={classes.submit}
+                    // className={classes.submit}
                 >
                     Save
                 </Button>
-            </form>
-        </div>
+                <Tooltip
+                    title='Get your token at https://github.com/settings/tokens witn repo and delete repo permissions.'
+                    className={classes.tooltip}
+                >
+                    <HelpIcon color='primary' />
+                </Tooltip>
+            </div>
+        </form>
     );
 };
 
